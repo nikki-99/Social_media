9b34cf3639257329e3d17d12d5d46a88ed32611c
Nikita <nikitadasmsd@gmail.com>
2021-03-05 16:11:27 +0530

    commit

--- contactform/__init__.py
+++ contactform/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -17,6 +18,8 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'notice'
+migrate = Migrate(app,db)
+
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
